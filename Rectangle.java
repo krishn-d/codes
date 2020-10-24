360e3a54928ee031b7ae2075b2b6d588a276862b
krishn-d <72937368+krishn-d@users.noreply.github.com>
2020-10-24 10:15:10 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle
+{
+	public static void main(String[] params)
+	{
+		System.out.println("Main of Rectangle is invoked");
+		Triangle.main(params);
+		System.out.println("Main of Rectangle is terinated");
+	}
+}
---
+++ Triangle.java
@@ -0,0 +1,9 @@
+public class Triangle
+{
+	public static void main(String[] shapes)
+	{
+		System.out.println("Data at index 0: "+shapes[0]);
+		System.out.println("Data at index 1: "+shapes[1]);
+	}
+}
+	
